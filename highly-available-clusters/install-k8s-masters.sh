b8856468acfe734c4ba316a8a39ba94430cf421b
Pranay Shah <pshah@tibco.com>
2020-09-28 14:29:52 -0700

    updated HA Scripts

--- highly-available-clusters/install-k8s-masters.sh
+++ highly-available-clusters/install-k8s-masters.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 
+# This script is to install Kubernetes in Hight avalabllity setup
+# This script assumes you have working load balanacer setup
+# This script assumes load balanacer and first kubernetes controle plane will be runnig on same machine
+# This script assumes second master node / controle-plane will be on machine name node01 and we can SSH to that server without any credentails
 
-LB_IP=$(dig controlplane +short)
+
+FIRST_MACHINE_NAME=controlplane
+SECOND_MACHINE_NAME=node01
+KUBERNETES_VERSION=1.19.0-00
+
+LB_PORT=9443
+LB_IP=$(dig $FIRST_MACHINE_NAME +short)
+
 
 if [ -n "$LB_IP" ];
  then
     echo "";
  else
-    echo "Could not find LB_IP from hostname 'controlplane' trying using hostname command now";
+    echo "Could not find LB_IP from hostname '$FIRST_MACHINE_NAME' trying using hostname command now";
     LB_IP=$(hostname -I | cut -d " " -f 1)
 fi
 
-LB_PORT=9443
 
-apt-get update && apt-get install -y kubeadm=1.19.0-00
-apt-get install -y kubelet=1.19.0-00 kubectl=1.19.0-00
+
+nc -z "$LB_IP" $LB_PORT || { echo "Issue connectiong to load balanacer @ $LB_IP:$LB_PORT - this needs to be invastigated"; exit 1; }
+
+# Optional if you need to update kubeadm,kubelet use these two lines (as of now 1.19)
+#apt-get update && apt-get install -y kubeadm=$KUBERNETES_VERSION
+#apt-get install -y kubelet=$KUBERNETES_VERSION kubectl=$KUBERNETES_VERSION
 
 echo "Starting to install kubernetes"
 echo "Command is [sudo kubeadm init --control-plane-endpoint $LB_IP:$LB_PORT --upload-certs --pod-network-cidr=10.244.0.0/16]"
@@ -74,20 +88,23 @@ echo "Starting to work on second controleplane on node machine"
 echo ""
 echo "Upgrading kubeadm,kubelet and kubectl on remote machine"
 echo ""
-ssh node01 apt-get update && apt-get install -y kubeadm=1.19.0-00
-ssh node01 apt-get install -y kubelet=1.19.0-00 kubectl=1.19.0-00
+
+# SSH into remote host and run commands
+# comment out next wo lines if you don't want to update kubeadm , kubelet on remote machine to be at $KUBERNETES_VERSION
+ssh $SECOND_MACHINE_NAME apt-get update && apt-get install -y kubeadm=$KUBERNETES_VERSION
+ssh $SECOND_MACHINE_NAME apt-get install -y kubelet=$KUBERNETES_VERSION kubectl=$KUBERNETES_VERSION
 
 echo "Generating joining command that remote kubeadm can use to join this cluster"
-#CERT_KEY=$(kubeadm alpha certs certificate-key)
+#CERT_KEY=$(kubeadm alpha certs certificate-key) <-- This should work in 1.19 but it does not (or failed for me)
 CERT_KEY=$(kubeadm init phase upload-certs --upload-certs | sed -n 3p)
-JOIN_COMMAND=$(kubeadm token create --print-join-command --certificate-key "$CERT_KEY")
+CONTROL-PLANE_JOIN_COMMAND=$(kubeadm token create --print-join-command --certificate-key "$CERT_KEY")
 #
 
-echo "JOIN COMMAND is [$JOIN_COMMAND]"
-echo $JOIN_COMMAND > join.text
+echo "JOIN COMMAND is [$CONTROL-PLANE_JOIN_COMMAND]"
+echo $CONTROL-PLANE_JOIN_COMMAND > join.text
 echo "Running joining command from remote machine"
 echo ""
-ssh node01 "$JOIN_COMMAND"
+ssh $SECOND_MACHINE_NAME "$CONTROL-PLANE_JOIN_COMMAND"
 
 
 echo "Checking the status of master nodes"
