3298fea1b06c8a1acaeb37d6c67a0a8b05c89e81
Pranay <pranay.shah@gmail.com>
2020-09-28 06:44:40 +0000

    added master install script

--- highly-available-clusters/install-k8s-master.sh
+++ highly-available-clusters/install-k8s-master.sh
@@ -4,6 +4,9 @@
 LB_IP=$(dig controlplane +short)
 LB_PORT=9443
 
+apt-get update && apt-get install -y kubeadm=1.19.0-00
+apt-get install -y kubelet=1.19.0-00 kubectl=1.19.0-00
+
 sudo kubeadm init --control-plane-endpoint "$LB_IP:$LB_PORT" --upload-certs --pod-network-cidr=10.244.0.0/16
 
 mkdir -p $HOME/.kube sudo
@@ -12,6 +15,19 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 curl https://docs.projectcalico.org/manifests/canal.yaml -O
 kubectl apply -f canal.yaml
+
+kubectl get nodes 
+
+ssh node01 apt-get update && apt-get install -y kubeadm=1.19.0-00
+ssh node01 apt-get install -y kubelet=1.19.0-00 kubectl=1.19.0-00
+
+JOIN_COMMAND=$(kubeadm token create --print-join-command)
+JOIN_COMMAND=$JOIN_COMMAND --control-plane
+
+echo "JOIN COMMAND is $JOIN_COMMAND"
+ssh node01 $JOIN_COMMAND
+
+
 
 
 
